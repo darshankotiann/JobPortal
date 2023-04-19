49d96afc7738ac261e82b9ec2d099c4c42e62dc3
darshankotiann <darshankotiann@gmail.com>
2023-04-19 21:56:33 +0530

    Firebase Auth Changed

--- super-admin/firebase/firebase_config.js
+++ super-admin/firebase/firebase_config.js
@@ -1,9 +1,20 @@
+// const firebaseConfig = {
+// 	apiKey: "AIzaSyCtCm4k-p64FohWyAqOJCh0bibcKEv40xk",
+// 	authDomain: "jobportal-d402a.firebaseapp.com",
+// 	projectId: "jobportal-d402a",
+// 	storageBucket: "jobportal-d402a.appspot.com",
+// 	messagingSenderId: "30163725727",
+// 	appId: "1:30163725727:web:c527918d8089c2848a21aa"
+//   };
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyCtCm4k-p64FohWyAqOJCh0bibcKEv40xk",
-	authDomain: "jobportal-d402a.firebaseapp.com",
-	projectId: "jobportal-d402a",
-	storageBucket: "jobportal-d402a.appspot.com",
-	messagingSenderId: "30163725727",
-	appId: "1:30163725727:web:c527918d8089c2848a21aa"
+	apiKey: "AIzaSyBazZbl5V_JOxwo4zo70ivsKXl-PUIBI9c",
+	authDomain: "jobportal-3a075.firebaseapp.com",
+	projectId: "jobportal-3a075",
+	storageBucket: "jobportal-3a075.appspot.com",
+	messagingSenderId: "958751418418",
+	appId: "1:958751418418:web:c9c5c5191db58c3858b42b",
+	measurementId: "G-57L0ZVTHQ8"
   };
 const app = firebase.initializeApp(firebaseConfig);
